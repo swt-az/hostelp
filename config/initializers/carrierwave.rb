26e3ff0011755fea6400fa67028b9df4478e313c
Tomoki <tomoki@Tom-MBP.local>
2017-02-23 22:39:27 +0900

    cw fixing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-    config.storage = :fog
+    # config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
