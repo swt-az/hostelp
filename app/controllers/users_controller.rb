7b3ae8d85c90bf4b244d8670376fade7ba33f6d7
Tomoki <tomoki@Tom-MBP.local>
2017-02-26 14:32:24 +0900

    ブランチの実験

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @rooms = Room.where(user_name: current_user.nickname)
-    # binding.pry
     @number = 1
   end
 end
